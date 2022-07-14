a39f28394f4e711f716a57a57d2467481f3008c8
Finn Welsford-Ackroyd <finnwa24@gmail.com>
2022-07-15 00:50:40 +1200

    Add grocy product barcode api

--- src/dev.ts
+++ src/dev.ts
@@ -1,30 +1,47 @@
 /* eslint-disable */
 
-import prompts from "prompts";
 import { GrocerSearchService } from "./grocer/search/grocer-search-service";
 import { GrocerStoreService } from "./grocer/stores/grocer-store-service";
-import { GrocyShoppingLocationService } from "./grocy/shopping-locations/grocy-shopping-location-service";
+import { grocyServices } from "./grocy";
 import { generateTypes } from "./jtd/generate-types";
 
+async function generate() {
+  const grocy = await grocyServices();
+  const barcodes = await grocy.productService.getProductBarcodes();
+  await generateTypes("ProductBarcodes", "src/grocy/products", barcodes);
+}
+
 export async function dev() {
+  // return generate();
   const storeService = new GrocerStoreService();
   const stores = await storeService.promptForStores();
   const search = new GrocerSearchService();
-  const { query } = await prompts({
-    type: "text",
-    name: "query",
-    message: "Search for a product",
-  });
-  await search.searchAndSelectProduct(
-    query as string,
-    stores.map((store) => store.id)
-  );
-}
-
-async function _generate() {
-  const shoppingLocationService = new GrocyShoppingLocationService();
-  const locations = await shoppingLocationService.getShoppingLocations();
-  await generateTypes("ShoppingLocations", "src/grocy/shopping-locations", locations);
+  // const { query } = await prompts({
+  //   type: "text",
+  //   name: "query",
+  //   message: "Search for a product",
+  // });
+  const grocy = await grocyServices();
+  const barcodes = await grocy.productService.getProductBarcodes();
+  const productsWithBarcodes = barcodes.map((b) => b.product_id);
+  for (const product of await grocy.productService.getAllProducts()) {
+    if (
+      product.barcode ||
+      product.name.includes("(Generic)") ||
+      productsWithBarcodes.includes(product.id)
+    ) {
+      continue;
+    }
+    console.log(product.name);
+    const grocerProduct = await search.searchAndSelectProduct(
+      product.name,
+      stores.map((store) => store.id)
+    );
+    if (grocerProduct) {
+      console.log(grocerProduct.id);
+      await grocy.productService.addProductBarcode(product.id, grocerProduct.id.toString());
+    }
+  }
 }
 
 /* eslint-enable */
