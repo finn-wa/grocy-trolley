a4bf4c6075e7c6757b671f64d6d2416b40a5cbb7
Finn Welsford-Ackroyd <finnwa24@gmail.com>
2022-07-17 02:31:59 +1200

    wip: process products, add barcode to those that don't have one

--- src/grocer/grocy/export/grocer-grocy-shopping-list-exporter.ts
+++ src/grocer/grocy/export/grocer-grocy-shopping-list-exporter.ts
@@ -1,3 +1,7 @@
+import { GrocerSearchService } from "@gt/grocer/search/grocer-search-service";
+import { GrocerStoreService } from "@gt/grocer/stores/grocer-store-service";
+import { GrocyServices } from "@gt/grocy";
+import { ShoppingListItem } from "@gt/grocy/shopping-lists/types/ShoppingListItems";
 import { Logger } from "@gt/utils/logger";
 
 export class GrocerShoppingListService {
@@ -7,4 +11,48 @@ export class GrocerShoppingListService {
   // resolve child products of parents
   // how to transfer to user browser though?
   // generate script?
+
+  constructor(
+    private readonly grocy: Pick<
+      GrocyServices,
+      "productService" | "parentProductService" | "shoppingListService"
+    >,
+    private readonly searchService: GrocerSearchService,
+    private readonly storeService: GrocerStoreService
+  ) {}
+
+  async importGrocyShoppingList(shoppingListId?: string, storeIds?: number[]) {
+    if (!shoppingListId) {
+      const id = await this.grocy.shoppingListService.selectShoppingList();
+      if (id === null) {
+        return;
+      }
+      shoppingListId = id;
+    }
+    const shoppingList = await this.grocy.shoppingListService.getShoppingList(shoppingListId);
+    if (!storeIds) {
+      const stores = await this.storeService.promptForStores();
+      storeIds = stores.map((store) => store.id);
+    }
+    const parentProducts = await this.grocy.parentProductService.getParentProducts();
+    for (const item of shoppingList.items) {
+      if (item.product_id in parentProducts) {
+        continue;
+      }
+      const product = await this.grocy.productService.getProduct(item.product_id);
+      if (!product.barcode) {
+        console.log(`No barcode found for "${product.name}", searching grocer`);
+        const grocerProduct = await this.searchService.searchAndSelectProduct(
+          product.name,
+          storeIds
+        );
+        if (grocerProduct === null) {
+          return;
+        }
+        this.logger.debug(`Adding barcode ${grocerProduct.id} }to grocy product...`);
+        // not working
+        await this.grocy.productService.addProductBarcode(product.id, grocerProduct.id.toString());
+      }
+    }
+  }
 }
